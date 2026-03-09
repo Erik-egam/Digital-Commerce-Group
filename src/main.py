4bbb6d6d79cae31b4fc3a05aa8455fbfb4864dc7
Acosta1001 <158068182+Acosta1001@users.noreply.github.com>
2026-03-09 02:34:50 -0500

    resolviendo conflictos en main.py

--- src/main.py
+++ src/main.py
@@ -1,13 +1,8 @@
-from fastapi import FastAPI, Depends
-from sqlalchemy.orm import Session
-from config.sql_alquemy_conexion import get_db
-from models.orm.cliente import Cliente
+from fastapi import FastAPI
 from config.conexion_mysql import crear_conexion
 from clientes import routes as clientes_routes
 from productos import routes as productos_routes
 from pedidos import routes as pedidos_routes
-from models.orm.pedido import Pedido
-from models.orm.producto import Producto
 
 app = FastAPI()
 
